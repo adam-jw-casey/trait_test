26856ed88efcd31edee554709e991a61039930b6
Adam Casey <adam.jw.casey@gmail.com>
2023-12-12 22:32:40 -0700

    Remove commented out references to Arbitrary and move example into lib

--- src/lib.rs
+++ src/lib.rs
@@ -13,6 +13,20 @@ pub fn derive_tested_trait(args: TokenStream, input: TokenStream) -> TokenStream
     input
 }
 
+//#[derive_tested_trait(Container)]
+//mod container_tests {
+//    use super::UntestedContainer;
+//
+//    #[test]
+//    fn test_returns_correct_num_items() {
+//        #[arbitrary(default)]
+//        let c: Container<usize> = Container::new(Box::new([0,1,2,3]));
+//
+//        assert_eq!(c.len(), c.into_iter().count());
+//
+//    }
+//}
+
 // This is what should be produced from the above
 #[proc_macro_derive(Container)]
 pub fn derive(input: TokenStream) -> TokenStream {
@@ -24,19 +38,15 @@ pub fn derive(input: TokenStream) -> TokenStream {
     let (impl_generics, ty_generics, where_clause) = input.generics.split_for_impl();
 
     let expanded = quote! {
-        // #[derive(Arbitrary)] TODO
-
         impl #impl_generics Container #impl_generics for #type_name #ty_generics #where_clause{}
 
         #[cfg(test)]
         mod #test_mod_name {
             use super::UntestedContainer;
             use super::#type_name;
-            //use arbitrary::Arbitrary; TODO
             
             #[test]
             fn test_returns_correct_num_items() {
-                // #[arbitrary(default)] TODO
                 let c: #type_name<usize> = #type_name::new(Box::new([0,1,2,3]));
 
                 assert_eq!(c.len(), c.into_iter().count());
--- tests/test1.rs
+++ tests/test1.rs
@@ -7,20 +7,6 @@ trait UntestedContainer <T>: IntoIterator{
 
 trait Container <T>: UntestedContainer<T>{}
 
-//#[derive_tested_trait(Container)]
-//mod container_tests {
-//    use super::UntestedContainer;
-//    
-//    #[test]
-//    fn test_returns_correct_num_items() {
-//        #[arbitrary(default)]
-//        let c: Container<usize> = Container::new(Box::new([0,1,2,3]));
-//
-//        assert_eq!(c.len(), c.into_iter().count());
-//
-//    }
-//}
-
 impl <T> UntestedContainer<T> for Pile<T>{
     fn new (items: Box<[T]>) -> Self {
         Self{contents: Vec::from(items)}
